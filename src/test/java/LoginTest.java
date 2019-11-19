59495ebbcd1d7fafca41ba517953ef1ee0f4c1a5
Roxana <filip.roxana95@gmail.com>
2019-11-19 12:34:53 +0200

    Automation 1

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+
+    @Test
+    public void login_test() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        WebElement accountLink = driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
+        accountLink.click();
+
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+
+        driver.findElement(By.cssSelector("#email")).sendKeys("filip.roxana95@gmail.com");
+
+        driver.findElement(By.cssSelector("#pass")).sendKeys("11martie11");
+
+        driver.findElement(By.cssSelector("#send2")).click();
+
+        driver.quit();
+
+    }
+
+
+}
---
+++ src/test/java/RegisterTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.apache.commons.lang3.RandomStringUtils.random;
+
+
+public class RegisterTest {
+
+    @Test
+    public void register() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://testfasttrackit.info/selenium-test/");
+
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a")).click();
+
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+
+        driver.findElement(By.id("firstname")).sendKeys("User");
+
+        driver.findElement(By.id("middlename")).sendKeys("I");
+
+        driver.findElement((By.id("lastname"))).sendKeys("User");
+
+        String email = random(5, true, false);
+
+        driver.findElement(By.id("email_address")).sendKeys(email + "@gmail.com");
+
+        driver.findElement(By.id("password")).sendKeys("1234.");
+
+        driver.findElement(By.id("confirmation")).sendKeys("1234.");
+
+        driver.findElement(By.cssSelector("#is_subscribed")).click();
+
+        driver.quit();
+
+
+    }
+    }
